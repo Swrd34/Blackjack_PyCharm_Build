61b3af6018937b21463a8b194d7774bc35b0f4d2
Swrd <swordartron34@gmail.com>
2025-10-23 00:43:09 -0400

    Better var

--- BlackjackPC/Blackjack Files/DeckSystem.py
+++ BlackjackPC/Blackjack Files/DeckSystem.py
@@ -17,7 +17,7 @@ deckCardValue = list(deck.values())  # Converts deck.values to a list and assign
 deckCardName = list(deck.keys())  # x Converts deck.keys to a list and assigns it to deckCardName
 
 
-def pull_Card1():
+def pull_Card():
     """Pulls a random card from the deck without changing the amount of values in deck. This returns a tuple"""
     x = random.randint(0,len(deck)-1) #Range between 0 and the amount of cards in deck
     if __name__ == "__main__" :
