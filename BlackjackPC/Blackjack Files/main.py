f7ac72e0e73c3893798df01099d5377ed85a809f
Swrd <swordartron34@gmail.com>
2025-10-23 03:48:54 -0400

    tests

--- BlackjackPC/Blackjack Files/main.py
+++ BlackjackPC/Blackjack Files/main.py
@@ -1,3 +1,10 @@
 import DeckSystem
-from DeckSystem import pull_Card1
+print("--------------First Pull----------------")
+print(DeckSystem.deck_Count())
+DeckSystem.deal_Card()
+print("--------------First Pull----------------\n\n")
+print("--------------Second Pull----------------")
+DeckSystem.deal_Card()
+print(DeckSystem.deck_Count())
+print("--------------Second Pull----------------")
 
