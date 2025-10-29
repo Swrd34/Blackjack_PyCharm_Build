768341ada9058b0daaacd19756d8e2dfa9251423
Swrd <swordartron34@gmail.com>
2025-10-29 11:26:20 -0400

    debug code

--- BlackjackPC/Blackjack Files/main.py
+++ BlackjackPC/Blackjack Files/main.py
@@ -1,3 +1,12 @@
 import DeckSystem
 
+def start_game():
+    DeckSystem.deal_card() #amount of cards left in deck should be 50
+
+    DeckSystem.deck_count()
+
+i = 0
+while i > 52:
+    DeckSystem.deal_card()
+    i += 1
 
