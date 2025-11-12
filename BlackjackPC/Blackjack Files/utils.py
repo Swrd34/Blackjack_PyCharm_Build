cb753f38ddeb9648a6663333685a5afa23c38fdc
Swrd <swordartron34@gmail.com>
2025-11-12 08:44:07 -0500

    *_*

--- BlackjackPC/Blackjack Files/utils.py
+++ BlackjackPC/Blackjack Files/utils.py
@@ -3,7 +3,7 @@ from time import sleep
 def calc_hand(values):
     """Calculates the total value of a hand"""
     total = sum(values)
-    if 1 in values and total + 10 <= 21: #An hour of insanity in just 4 lines of code. Sigh.
+    if 1 in values and total + 10 <= 21: #An hour of insanity in just 1 line of code. Sigh.
         total += 10
     return total
 
