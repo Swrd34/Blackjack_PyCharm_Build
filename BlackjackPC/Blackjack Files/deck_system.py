962039c47512e44df8ad552f6de5412822ade116
Swrd <swordartron34@gmail.com>
2025-11-12 08:47:47 -0500

    Symbols very cool

--- BlackjackPC/Blackjack Files/deck_system.py
+++ BlackjackPC/Blackjack Files/deck_system.py
@@ -4,19 +4,19 @@ import random
 import globals
 import utils
 
-deck = {"Ace of Hearts": 1, "Ace of Spades": 1, "Ace of Clubs": 1, "Ace of Diamonds": 1,  #Full deck of cards
-        "2 of Hearts": 2, "2 of Spades": 2, "2 of Clubs": 2, "2 of Diamonds": 2,
-        "3 of Hearts": 3, "3 of Spades": 3, "3 of Clubs": 3, "3 of Diamonds": 3,
-        "4 of Hearts": 4, "4 of Spades": 4, "4 of Clubs": 4, "4 of Diamonds": 4,
-        "5 of Hearts": 5, "5 of Spades": 5, "5 of Clubs": 5, "5 of Diamonds": 5,
-        "6 of Hearts": 6, "6 of Spades": 6, "6 of Clubs": 6, "6 of Diamonds": 6,
-        "7 of Hearts": 7, "7 of Spades": 7, "7 of Clubs": 7, "7 of Diamonds": 7,
-        "8 of Hearts": 8, "8 of Spades": 8, "8 of Clubs": 8, "8 of Diamonds": 8,
-        "9 of Hearts": 9, "9 of Spades": 9, "9 of Clubs": 9, "9 of Diamonds": 9,
-        "10 of Hearts": 10, "10 of Spades": 10, "10 of Clubs": 10, "10 of Diamonds": 10,
-        "Jack of Hearts": 10, "Jack of Spades": 10, "Jack of Clubs": 10, "Jack of Diamonds": 10,
-        "Queen of Hearts": 10, "Queen of Spades": 10, "Queen of Clubs": 10, "Queen of Diamonds": 10,
-        "King of Hearts": 10, "King of Spades": 10, "King of Clubs": 10, "King of Diamonds": 10}
+deck = {"♥A": 1, "♦A": 1, "♣A": 1, "♠A": 1,  #Full deck of cards
+        "♥2": 2, "♦2": 2, "♣2": 2, "♠2": 2,
+        "♥3": 3, "♦3": 3, "♣3": 3, "♠3": 3,
+        "♥4": 4, "♦4": 4, "♣4": 4, "♠4": 4,
+        "♥5": 5, "♦5": 5, "♣5": 5, "♠5": 5,
+        "♥6": 6, "♦6": 6, "♣6": 6, "♠6": 6,
+        "♥7": 7, "♦7": 7, "♣7": 7, "♠7": 7,
+        "♥8": 8, "♦8": 8, "♣8": 8, "♠8": 8,
+        "♥9": 9, "♦9": 9, "♣9": 9, "♠9": 9,
+        "♥10": 10, "♦10": 10, "♣10": 10, "♠10": 10,
+        "♥J": 10, "♦J": 10, "♣J": 10, "♠J": 10,
+        "♥Q": 10, "♦Q": 10, "♣Q": 10, "♠Q": 10,
+        "♥K": 10, "♦K": 10, "♣K": 10, "♠K": 10}
 
 
 def pull_card():
