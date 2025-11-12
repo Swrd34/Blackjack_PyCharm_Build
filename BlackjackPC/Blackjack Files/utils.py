e05423985f2419cf6fe78d2d827ce6851ecb94a9
Swrd <swordartron34@gmail.com>
2025-11-12 08:46:03 -0500

    Moved menu functions here cause it feels better

--- BlackjackPC/Blackjack Files/utils.py
+++ BlackjackPC/Blackjack Files/utils.py
@@ -13,3 +13,24 @@ def load():
         print(".", end="")
         sleep(0.8)
     print("\n")
+
+def get_user_input(choice_list: list[str]):
+    """Checks if user input matches given parameters and loops till it does"""
+    user_choice = (input().lower()).replace(" ", "") #Keeps user input not case sensitive and removes whitespace
+    while user_choice not in choice_list:
+        print(f"Invalid input. Try Again. Your choice: {user_choice}")
+        user_choice = (input().lower()).replace(" ", "")
+    return user_choice
+
+def main_menu():
+    """Prints main menu"""
+    print("\033[97mWelcome to Swrd's BlackJack Sim! Have fun!\n\033[97m")
+    print("Where would you like to go?")
+    print(">Play Game\n"
+          ">About\n"
+          
+          ">Exit\n")
+
+def about():
+    """About section"""
+    print("This is a little blackjack game I made to get some experience with programming in python! I hope you enjoy playing it as much as I enjoyed making it!")
